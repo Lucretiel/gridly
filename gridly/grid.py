ab8fd9545becbe6a3e15e718654dac80bf1f15ce
Nathan West <Lucretiel@gmail.com>
2013-12-03 02:15:41 -0500

    Removed copying. Various minor edits.
    Removed copying
    Added check_location
    Added comments

--- gridly/grid.py
+++ gridly/grid.py
@@ -1,47 +1,71 @@
-import copy
-
 class GridMixin:
+    '''
+    Mixin to provide common functionality to Grids. Grid concrete classes should
+    implement `unsafe_get`, `unsafe_set`, and `__init__(num_rows, num_columns,
+    content=content)`
+    '''
     def __init__(self, num_rows, num_columns, content):
         self.num_rows = num_rows
         self.num_columns = num_columns
         self.content = content
 
     def valid(self, location):
+        '''
+        Return true if a location is valid
+        '''
         return (0 <= location[0] < self.num_rows and
             0 <= location[1] < self.num_columns)
 
-    def __getitem__(self, location):
-        if self.valid(location):
-            return self.unsafe_get(location)
+    def check_location(self, location):
+        '''
+        Return location if a location is valid. Raise IndexError otherwise.
+        '''
+        #TODO: Check back here as a candidate for "inner loop" optimiztion
+        if self.valid(location)
+            return location
         else:
             raise IndexError(location)
+
+    def __getitem__(self, location):
+        return self.unsafe_set(self.check_location(location))
 
     def __setitem__(self, location, value):
-        if self.valid(location):
-            return self.unsafe_set(location, value)
-        else:
-            raise IndexError(location)
+        return self.unsafe_set(self.check_location(location))
 
     def row(self, row):
+        '''
+        Iterate over all the cells in a row
+        '''
         for column in range(self.num_columns):
             yield self.unsafe_get((row, column))
 
     def column(self, column):
+        '''
+        Iterate over all the cells in a column
+        '''
         for row in range(self.num_rows):
             yield self.unsafe_get((row, column))
 
     def rows(self):
+        '''
+        Iterate over each row. Each row is an iterable, containing each cell in
+        the row
+        '''
         for row in range(self.num_rows):
             yield self.row(row)
 
     def columns(self):
+        '''
+        Iterate over each columns. Each column is an iterable, containing each
+        cell in the column
+        '''
         for column in range(self.num_columns):
             yield self.column(column)
 
-    def copy(self):
-        return type(self)(self.num_rows, self.num_columns, content=copy.copy(self.content))
-
 class DenseGrid(GridMixin):
+    '''
+    DenseGrid is for grids which have content in most of the cells.
+    '''
     def __init__(num_rows, num_columns, fill=None, content=None):
         if content is None:
             content = [fill] * num_rows * num_columns
@@ -58,6 +82,10 @@ class DenseGrid(GridMixin):
 
 
 class SparseGrid(GridMixin):
+    '''
+    SparseGrid is for grids for which most of the cells are some empty, default
+    value
+    '''
     def __init__(num_rows, num_columns, fill=None, content=None):
         if content is None:
             content = {}
