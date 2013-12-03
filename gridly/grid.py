6ff14bbf1a158e7b4c5030b0090324d89bdccc0e
Nathan West <Lucretiel@gmail.com>
2013-12-03 01:57:31 -0500

    Added Grid, various updates
    Added grid submodule
    Minor style changes to location and direction
    Removed doctests
    Updated gitignore

---
+++ gridly/grid.py
@@ -0,0 +1,71 @@
+class GridMixin:
+    def __init__(self, num_rows, num_columns, content):
+        self.num_rows = num_rows
+        self.num_columns = num_columns
+        self.content = content
+
+    def valid(self, location):
+        return (0 <= location[0] < self.num_rows and
+            0 <= location[1] < self.num_columns)
+
+    def __getitem__(self, location):
+        if self.valid(location):
+            return self.unsafe_get(location)
+        else:
+            raise IndexError(location)
+
+    def __setitem__(self, location, value):
+        if self.valid(location):
+            return self.unsafe_set(location, value)
+        else:
+            raise IndexError(location)
+
+    def row(self, row):
+        for column in range(self.num_columns):
+            yield self.unsafe_get((row, column))
+
+    def column(self, column):
+        for row in range(self.num_rows):
+            yield self.unsafe_get((row, column))
+
+    def rows(self):
+        for row in range(self.num_rows):
+            yield self.row(row)
+
+    def columns(self):
+        for column in range(self.num_columns):
+            yield self.column(column)
+
+
+class DenseGrid(GridMixin):
+    def __init__(num_rows, num_columns, fill=None, content=None):
+        if content is None:
+            content = [fill] * num_rows * num_columns
+        GridMixin.__init__(self, num_rows, num_columns, content)
+
+    def index(self, location):
+        return (self.num_columns * location[0]) + location[1]
+
+    def unsafe_get(self, location):
+        return self.content[self.index(location)]
+
+    def unsafe_set(self, location, value):
+        self.content[self.index(location)] = value
+
+
+class SparseGrid(GridMixin):
+    def __init__(num_rows, num_columns, fill=None, content=None):
+        if content is None:
+            content = {}
+        GridMixin.__init__(self, num_rows, num_columns, content)
+        self.fill = fill
+
+    def unsafe_get(self, location):
+        return self.content.get(tuple(location), self.fill)
+
+    def unsafe_set(self, location, value):
+        location = tuple(location)
+        if value is self.fill:
+            self.content.pop(location, None)
+        else:
+            self.content[location] = value
--- gridly/location.py
+++ gridly/location.py
@@ -11,68 +11,47 @@ class Location(collections.namedtuple('Location', ('row', 'column'))):
     def zero(cls):
         '''
         Create a (0, 0) initialized Location
-
-        >>> Location.zero()
-        Location(row=0, column=0)
         '''
         return cls(0, 0)
 
     def __add__(self, other):
         '''
         Adds 2 Locations, memberwise.
-
-        >>> Location(1, 2) + Location(3, 4)
-        Location(row=4, column=6)
         '''
         return Location(self[0] + other[0], self[1] + other[1])
 
     def __sub__(self, other):
         '''
         Subtracts 2 Locations, memberwise.
-
-        >>> Location(4, 4) - Location(1, 2)
-        Location(row=3, column=2)
         '''
         return Location(self[0] - other[0], self[1] - other[1])
 
     def above(self, distance=1):
         '''
         Return the location above this one, at the specified distance
-
-        >>> Location(2, 3).above()
-        Location(row=1, column=3)
         '''
         return Location(self[0]-distance, self[1])
 
     def below(self, distance=1):
         '''
         Return the location below this one, at the specified distance
-
-        >>> Location(2, 3).below()
-        Location(row=3, column=3)
         '''
         return Location(self[0]+distance, self[1])
 
     def left(self, distance=1):
         '''
         Return the location to the left of this one, at the specified distance
-
-        >>> Location(2, 3).left()
-        Location(row=2, column=2)
         '''
         return Location(self[0], self[1]-distance)
 
     def right(self, distance=1):
         '''
         Return the location to the right of this one, at the specified distance
-
-        >>> Location(2, 3).right()
-        Location(row=2, column=4)
         '''
         return Location(self[0], self[1]+distance)
 
     #directions is a mapping of directions to the relative location functions above
-    directions = {
+    _directions = {
         Direction.up: above,
         Direction.down: below,
         Direction.left: left,
@@ -82,65 +61,25 @@ class Location(collections.namedtuple('Location', ('row', 'column'))):
     def relative(self, direction, distance=1):
         '''
         Returns the location in the direction specified, at the specified distance
-
-        >>> Location.zero().relative(Direction.down)
-        Location(row=1, column=0)
         '''
-        return Location.directions[direction](self, distance)
-
-    def path(self, *directions):
-        '''
-        Given 1 or more directions, follows all of them in sequence, and returns
-        the final location.
-
-        >>> Location.zero().path(Direction.down, Direction.right, Direction.down)
-        Location(row=2, column=1)
-        '''
-        loc = self
-        for d in directions:
-            loc = loc.relative(d)
-        return loc
-
-    def each_at(self, *directions):
-        '''
-        Given a series of directions or paths (where a path is an ordered
-        collection of directions), yield each relative directions.
-
-        >>> list(Location(1, 1).each_at(Direction.up, Direction.left, (Direction.right, Direction.down)))
-        [Location(row=0, column=1), Location(row=1, column=0), Location(row=2, column=2)]
-        '''
-        for direct in directions:
-            if isinstance(direct, Direction):
-                yield self.relative(direct)
-            else:
-                yield self.path(*direct)
+        return Location._directions[direction](self, distance)
 
     def adjacent(self):
         '''
-        Return a list of the 4 locations adjacent to self.
-
-        >>> Location(1, 1).adjacent()
-        [Location(row=1, column=2), Location(row=0, column=1), Location(row=1, column=0), Location(row=2, column=1)]
+        Return a set of the 4 locations adjacent to self.
         '''
-        return [self.relative(direction) for direction in Direction]
+        return {self.relative(direction) for direction in Direction}
+
     def diagonals(self):
         '''
-        Return a list of the 4 locations diagonaly adjacent to self
-
-        >>> Location(1, 1).diagonals()
-        [Location(row=0, column=0), Location(row=0, column=2), Location(row=2, column=0), Location(row=2, column=2)]
+        Return a set of the 4 locations diagonaly adjacent to self
         '''
-        return [self.path(dir1, dir2)
+        return {self.relative(dir1).relative(dir2)
             for dir1 in (Direction.up, Direction.down)
-            for dir2 in (Direction.left, Direction.right)]
+            for dir2 in (Direction.left, Direction.right)}
 
     def surrounding(self):
         '''
-        Yields each of the 8 locations surrounding self
+        Return a set of the 8 locations surrounding self
         '''
-
-        for loc in self.adjacent():
-            yield loc
-
-        for loc in self.diagonals():
-            yield loc
+        return self.adjacent() + self.diagonals()
