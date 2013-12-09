abf07e64cd11aa1188258186be0082b45f8a9724
Nathan West <Lucretiel@gmail.com>
2013-12-09 13:54:51 -0500

    Various organizational changes
    Added 3.2 to the classifiers list
    Rearranged python version lists
    Modified travis script to fix coverage reports

--- setup.py
+++ setup.py
@@ -19,10 +19,11 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2'
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
